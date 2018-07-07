72908cc95133d35d44d70cd0f6575518caf5c0a5
daniellaguips@gmail.com Guimaraes <daniellaguips@gmail.com>
2018-07-07 22:35:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (let i = 0; i < musicians.length; i ++ ){
+    array.push(musicians[i] + " plays " + instruments[i])
+  }
+  return array
+} 
+
+function johnLennonFacts(array){
+  var i = 0
+  while (i <= array.length-1, i++) {
+      newArray[i] = (array[i]  + "!!!")
+    }
+    return newArray
+}
